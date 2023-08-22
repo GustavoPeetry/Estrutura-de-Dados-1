74e85803f8f4f1950acbc90d878d367a061a07cb
Gustavo Petry <65690479+GustavoPeetry@users.noreply.github.com>
2023-08-22 15:46:13 -0300

    .

---
+++ Exercícios/Lista 1/ex2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int fatorial(int n)
+{
+    if (n==0)
+        return 1;
+
+    int x;
+    x = fatorial(n-1);
+    return n*x;
+}
+
+
+
+int main()
+{
+    int n = 20;
+
+    printf("%d", fatorial(n));
+
+
+    return 0;
+}
